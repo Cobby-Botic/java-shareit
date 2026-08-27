8460e5cc250a5f46b429b934772446f1b99b981e
Daniel <dan.cowaleff@yandex.ru>
2026-08-27 18:48:44 +0300

    Реализация классов

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,6 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
 public class ItemDto {
+    private String name;
+    private String description;
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
 public class Item {
+    private int id;
+    private String name;
+    private String description;
+    private int owner;
+    private int requestId;
 }
